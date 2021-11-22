ee23669f9727440efc7894415032374f98c6e525
as-amar <86281070+as-amar@users.noreply.github.com>
2021-11-23 00:26:21 +0530

    Add files via upload

---
+++ passwordGenerator.py
@@ -0,0 +1,12 @@
+import random
+
+lower = "abcdefghijklmnopqrstuvwxyz"
+upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+numbers = "012345678"
+symbols = "[]{}()*;/,_-"
+
+all = lower + upper + numbers + symbols
+length = 16
+password = "".join(random.sample(all,length))
+print(password)
+
